cdf14edfc72d7c7f7b6a677387acc91b3a40e654
Nick Babcock <nbabcock19@hotmail.com>
2015-04-23 18:42:32 -0400

    Init commit

---
+++ src/Pfim/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Pfim")>]
+[<assembly: AssemblyProductAttribute("Pfim")>]
+[<assembly: AssemblyDescriptionAttribute("Image file format parser")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
