b706965a749e33cce236e161033791ee935d1689
Nick Babcock <nbabcock19@hotmail.com>
2015-04-23 19:12:00 -0400

    Convert into a C# library

---
+++ src/Pfim/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pfim
+{
+    public class Class1
+    {
+        public const int a = 42;
+    }
+}
--- tests/Pfim.Tests/Tests.fs
+++ tests/Pfim.Tests/Tests.fs
@@ -5,6 +5,6 @@ open NUnit.Framework
 
 [<Test>]
 let ``hello returns 42`` () =
-  let result = Library.hello 42
+  let result = Class1.a
   printfn "%i" result
   Assert.AreEqual(42,result)
