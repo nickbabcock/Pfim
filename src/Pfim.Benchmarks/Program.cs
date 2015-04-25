4ad4da5d878b2078ec21d73158c4bc348b1a9edb
Nick Babcock <nbabcock19@hotmail.com>
2015-04-25 14:47:48 -0400

    Direct comparison against targaimage

--- src/Pfim.Benchmarks/Program.cs
+++ src/Pfim.Benchmarks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SimpleSpeedTester.Core;
+using SimpleSpeedTester.Core.OutcomeFilters;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,20 +11,18 @@ namespace Pfim.Benchmarks
 {
     class Program
     {
-        static MemoryStream mem = new MemoryStream(File.ReadAllBytes(Path.Combine("data", "true-24.tga")));
+        static byte[] data = File.ReadAllBytes(Path.Combine("data", "true-24.tga"));
         static void Main(string[] args)
         {
-            // initialize a new test group
-            var testGroup = new TestGroup("Example2");
+            var testGroup = new TestGroup("Targa 24");
 
-            // PlanAndExecute actually executes the Action delegate 5 times and returns the result summary
-            var testResultSummary = testGroup.PlanAndExecute("Test1", () =>
-            { 
-                Targa.Create(mem);
-                mem.Seek(0, SeekOrigin.Begin);
-            }, 5);
+            var pfim = testGroup.Plan("Pfim", () => Targa.Create(new MemoryStream(data)), 5);
 
-            Console.WriteLine(testResultSummary);
+            var targaImage = testGroup.Plan("TargaImage", () =>
+                new Paloma.TargaImage(new MemoryStream(data)), 5);
+
+            Console.WriteLine(pfim.GetResult().GetSummary(new ExcludeMinAndMaxTestOutcomeFilter()));
+            Console.WriteLine(targaImage.GetResult().GetSummary(new ExcludeMinAndMaxTestOutcomeFilter()));
         }
 
     }
