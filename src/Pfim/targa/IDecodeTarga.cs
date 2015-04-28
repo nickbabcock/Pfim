36067af5e082f8f273d6512feddc5fbc7ce2aeec
Nick Babcock <nbabcock19@hotmail.com>
2015-04-28 16:05:05 -0400

    Describe IDecodeTarga interface

--- src/Pfim/targa/IDecodeTarga.cs
+++ src/Pfim/targa/IDecodeTarga.cs
@@ -2,14 +2,23 @@
 
 namespace Pfim
 {
+    /// <summary>
+    /// Interface describe a class that is able to decode a targa image that may have its pixel
+    /// start anywhere. For instance, in a bottom left scenario, the first pixel read corresponds to
+    /// the first pixel on the last row
+    /// </summary>
     internal interface IDecodeTarga
     {
+        /// <summary>Decode pixels starting at bottom left</returns>
         byte[] BottomLeft(Stream str, TargaHeader header);
 
+        /// <summary>Decode pixels starting at bottom right</returns>
         byte[] BottomRight(Stream str, TargaHeader header);
 
+        /// <summary>Decode pixels starting at top right</returns>
         byte[] TopRight(Stream str, TargaHeader header);
 
+        /// <summary>Decode pixels starting at top left</returns>
         byte[] TopLeft(Stream str, TargaHeader header);
     }
 }
