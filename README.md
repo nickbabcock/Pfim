8871b20b89237cab32df9dd3eebcdcf638f8da9b
Nick Babcock <nbabcock19@hotmail.com>
2015-04-29 11:16:10 -0400

    Documentation for the site

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 
 # Pfim
 
+Pfim is an incredibly simple and fast image decoding library with an emphasis
+on being backend and frontend agnostic. This means that you can add Pfim to
+your server, Windows Form, or WPF app!
+
+[Main documention page](http://nickbabcock.github.io/Pfarah/)
+
 - [Targa image specification](http://www.dca.fee.unicamp.br/~martino/disciplinas/ea978/tgaffs.pdf)
 - [Block compression](https://msdn.microsoft.com/en-us/library/bb694531(v=vs.85).aspx) (useful for dds)
 - [DXT Compression Explained](http://www.fsdeveloper.com/wiki/index.php?title=DXT_compression_explained)
--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -4,10 +4,10 @@
 #I "../../bin"
 
 (**
-Pfim
-======================
+# Pfim
 
-Documentation
+Pfim is an incredibly simple and fast image decoding library with an emphasis
+on being backend and frontend agnostic.
 
 <div class="row">
   <div class="span1"></div>
@@ -20,27 +20,31 @@ Documentation
   <div class="span1"></div>
 </div>
 
-Samples & Documentation
------------------------
+## Getting Started
 
- * [Tutorial](tutorial.html) contains a further explanation of this sample library.
+    [lang=csharp]
+    // Load image from file path
+    IImage image = Pfim.FromFile(@"C:\image.tga");
+
+## Samples & Documentation
+
+ * [Tutorial](tutorial.html) contains a walkthrough some of the API for more uses.
 
  * [API Reference](reference/index.html) contains automatically generated documentation for all types, modules
    and functions in the library. This includes additional brief samples on using most of the
    functions.
  
-Contributing and copyright
---------------------------
+## Contributing and copyright
 
 The project is hosted on [GitHub][gh] where you can [report issues][issues],
-fork  the project and submit pull requests. If you're adding a new public API,
+fork the project and submit pull requests. If you're adding a new public API,
 please also  consider adding [samples][content] that can be turned into a
 documentation. You might also want to read the [library design notes][readme]
 to understand how it works.
 
 The library is available under the MIT license, which allows modification and
 redistribution for both commercial and non-commercial purposes. For more
-information see the  [License file][license] in the GitHub repository.
+information see the [License file][license].
 
   [content]: https://github.com/fsprojects/Pfim/tree/master/docs/content
   [gh]: https://github.com/fsprojects/Pfim
