cfae9addd038a234532419aa00920d6f4f396882
Nick Babcock <nbabcock19@hotmail.com>
2015-04-29 13:20:38 -0400

    Assembly infos updated

--- src/Pfim/Properties/AssemblyInfo.cs
+++ src/Pfim/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Pfim")]
 [assembly: AssemblyProductAttribute("Pfim")]
 [assembly: AssemblyDescriptionAttribute("Image file format parser")]
-[assembly: AssemblyVersionAttribute("0.1")]
-[assembly: AssemblyFileVersionAttribute("0.1")]
+[assembly: AssemblyVersionAttribute("0.2")]
+[assembly: AssemblyFileVersionAttribute("0.2")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.1";
+        internal const string Version = "0.2";
     }
 }
