c5a42e4397e101aca2fc3cdffaa73fad34186631
Nick Babcock <nbabcock19@hotmail.com>
2015-04-30 16:33:03 -0400

    Bump version to 0.3

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,7 @@
+### 0.3 - April 30 2015
+* Internalized a lot of API to simplify usage
+* Publish benchmarking
+
 ### 0.2 - April 29 2015
 * All decoded images now derive from `IImage`
 
--- src/Pfim.Bench/AssemblyInfo.fs
+++ src/Pfim.Bench/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Pfim.Bench")>]
 [<assembly: AssemblyProductAttribute("Pfim")>]
 [<assembly: AssemblyDescriptionAttribute("Image file format parser")>]
-[<assembly: AssemblyVersionAttribute("0.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.2")>]
+[<assembly: AssemblyVersionAttribute("0.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2"
+    let [<Literal>] Version = "0.3"
