4f64436a7fc1dfacfc4b0304c99d608755c83f6e
Nick Babcock <nbabcock19@hotmail.com>
2017-10-13 17:18:45 -0500

    Add Pfim.Viewer.Forms for windows forms sample

---
+++ src/Pfim.Viewer.Forms/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Pfim.Viewer.Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(12, 12);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(260, 23);
+            this.button.TabIndex = 0;
+            this.button.Text = "Open File";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(260, 208);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Pfim.Viewer.Forms";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ src/Pfim.Viewer.Forms/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace Pfim.Viewer.Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Images  (*.tga;*.dds)|*.tga;*.dds|All files (*.*)|*.*",
+                Title = "Open File with Pfim"
+
+            };
+
+            if (dialog.ShowDialog(this) != DialogResult.OK)
+            {
+                return;
+            }
+
+            var image = Pfim.FromFile(dialog.FileName);
+
+            PixelFormat format;
+            switch (image.Format)
+            {
+                case ImageFormat.Rgb24:
+                    format = PixelFormat.Format24bppRgb;
+                    break;
+
+                case ImageFormat.Rgba32:
+                    format = PixelFormat.Format32bppArgb;
+                    break;
+
+                default:
+                    throw new Exception("Format not recognized");
+            }
+
+            unsafe
+            {
+                fixed (byte* p = image.Data)
+                {
+                    var bitmap = new Bitmap(image.Width, image.Height, image.Stride, format, (IntPtr) p);
+                    pictureBox.Image = bitmap;
+                }
+            }
+        }
+    }
+}
