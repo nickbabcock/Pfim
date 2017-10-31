b5ef7dc953441261cd893f3fa2cf8f3f228a2473
Nick Babcock <nbabcock19@hotmail.com>
2017-10-31 17:49:44 -0500

    Include top left encoded targa in benchmark

--- src/Pfim.Benchmarks/TargaBenchmark.cs
+++ src/Pfim.Benchmarks/TargaBenchmark.cs
@@ -8,7 +8,7 @@ namespace Pfim.Benchmarks
 {
     public class TargaBenchmark
     {
-        [Params("true-32-rle-large.tga", "true-24-large.tga", "true-24.tga", "true-32-rle.tga")]
+        [Params("true-32-rle-large.tga", "true-24-large.tga", "true-24.tga", "true-32-rle.tga", "rgb24_top_left")]
         public string Payload { get; set; }
 
         private byte[] data;
