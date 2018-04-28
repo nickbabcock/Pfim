8d49509c0081fb415edba2d9b377646459eccd31
Nick Babcock <nbabcock19@hotmail.com>
2018-04-27 19:07:47 -0500

    Update analysis to include memory graphs

--- analysis.R
+++ analysis.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(readr)
+library(gdata)
 
 # Combine the targa and dds benchmarks into a single data frame
 df <- rbind(read_csv("TargaBenchmark-report.csv"), read_csv("DdsBenchmark-report.csv"))
@@ -22,14 +23,14 @@ payload_name <- Vectorize(function(payload) {
 # For each image, group all the contestants together and compute the relative
 # throughput for each one by taking their median time to decode and dividing
 # it by the fastest median in the group.
-df <- df %>% mutate(Payload = payload_name(Payload)) %>%
+throughput <- df %>% mutate(Payload = payload_name(Payload)) %>%
   group_by(Payload) %>%
   mutate(Relative = min(Median) / Median) %>%
   ungroup() %>%
   select(Method, Payload, Relative) %>%
   complete(Method, Payload)
 
-ggplot(df, aes(Method, Payload)) +
+ggplot(throughput, aes(Method, Payload)) +
   geom_tile(aes(fill = Relative), color = "white") +
   scale_x_discrete(position = "top") +
   scale_fill_gradient(name = "Throughput", low = "white", high = "steelblue", na.value = "#D8D8D8") +
@@ -38,3 +39,37 @@ ggplot(df, aes(Method, Payload)) +
   geom_text(aes(label = ifelse(is.na(Relative), "NA", format(round(Relative, 2), digits = 3)))) +
   ggtitle("Image Decoding with Relative Throughput",
           subtitle = "For Targa and Direct Draw Surface Images on the .NET platform. (Blue = Highest Throughput)")
+
+memory <- df %>% mutate(Payload = payload_name(Payload)) %>%
+  filter(!(Method %in% c("DevILSharp", "FreeImage", "ImageMagick"))) %>%
+  group_by(Payload) %>%
+  mutate(Relative = Allocated /  min(Allocated)) %>%
+  ungroup() %>%
+  select(Method, Payload, Relative, Allocated) %>%
+  complete(Method, Payload)
+
+ggplot(memory, aes(Method, Payload)) +
+  geom_tile(aes(fill = Relative), color = "white") +
+  scale_x_discrete(position = "top") +
+  scale_fill_gradient(name = "Relative ↑", low = "white", high = "#E63946", na.value = "#D8D8D8") +
+  xlab("Decoding Library") +
+  ylab("Image Decoded") +
+  geom_text(aes(label = ifelse(is.na(Relative), "NA", format(round(Relative, 2), digits = 3)))) +
+  ggtitle("Image Decoding with Relative Memory Allocation",
+          subtitle = "For Targa and Direct Draw Surface Images on the .NET platform.\nRelative allocations for a given image (red = more allocations)") +
+  labs(caption = ".NET dependencies that use native code for decoding are not shown")
+
+format_allocated <- Vectorize(function(alloc) {
+  ifelse(is.na(alloc), "NA", humanReadable(alloc, standard = "SI", digits = 0))
+})
+
+ggplot(memory, aes(Method, Payload)) +
+  geom_tile(aes(fill = Allocated), color = "white") +
+  scale_x_discrete(position = "top") +
+  scale_fill_gradient(name = "Memory", low = "white", high = "#E63946", na.value = "#D8D8D8", labels = format_allocated) +
+  xlab("Decoding Library") +
+  ylab("Image Decoded") +
+  geom_text(aes(label = format_allocated(Allocated))) +
+  ggtitle("Image Decoding with Total Memory Allocation",
+          subtitle = "For Targa and Direct Draw Surface Images on the .NET platform") +
+  labs(caption = ".NET dependencies that use native code for decoding are not shown")
