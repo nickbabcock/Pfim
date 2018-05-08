8d4c737f1a56c6dc1fa50795694cfc72a60b12c7
Nick Babcock <nbabcock19@hotmail.com>
2018-05-08 18:07:44 -0500

    Currently only allow native target format

--- src/Pfim/TargetFormat.cs
+++ src/Pfim/TargetFormat.cs
@@ -6,14 +6,6 @@ namespace Pfim
 {
     public enum TargetFormat
     {
-        Rgb8,
-        R5g5b5,
-        R5g6b5,
-        R5g5b5a1,
-        Rgba16,
-        Rgb24,
-        Rgba32,
-        Native,
-        NearestNeighbor
+        Native
     }
 }
