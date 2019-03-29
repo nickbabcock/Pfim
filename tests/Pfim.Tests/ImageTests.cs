80a70352a2cc70305865891feee5a8087df40c8e
Nick Babcock <nbabcock19@hotmail.com>
2019-03-29 16:33:55 -0500

    Refactor tests

---
+++ tests/Pfim.Tests/ImageTests.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using Xunit;
+using static Farmhash.Sharp.Farmhash;
+
+
+namespace Pfim.Tests
+{
+    public class ImageTests
+    {
+        [Theory]
+        [InlineData("24-bit-uncompressed-odd.dds", 1312155254599784230, ImageFormat.Rgb24)]
+        [InlineData("32-bit-uncompressed-odd.dds", 431228091138896641, ImageFormat.Rgba32)]
+        [InlineData("32-bit-uncompressed.dds", 8563937048591661181, ImageFormat.Rgba32)]
+        [InlineData("Antenna_Metal_0_Normal.dds", 11008763962346865605, ImageFormat.Rgb24)]
+        [InlineData("CBW8.tga", 11906269729452326966, ImageFormat.Rgb8)]
+        [InlineData("CCM8.tga", 11437704396291630213, ImageFormat.R5g5b5)]
+        [InlineData("CTC16.tga", 11437704396291630213, ImageFormat.R5g5b5)]
+        [InlineData("CYA.tga", 4966848095323908957, ImageFormat.Rgb24)]
+        [InlineData("DSCN1910_24bpp_uncompressed_10_2.tga", 1527117886507111010, ImageFormat.Rgb24)]
+        [InlineData("DSCN1910_24bpp_uncompressed_10_3.tga", 1527117886507111010, ImageFormat.Rgb24)]
+        [InlineData("TestVolume_Noise3D.dds", 12008948602044967211, ImageFormat.Rgba32)]
+        [InlineData("dds_A4R4G4B4.dds", 10024159200024540560, ImageFormat.Rgba16)]
+        [InlineData("dds_A8B8G8R8.dds", 10380399580803577188, ImageFormat.Rgba32)]
+        [InlineData("dds_R5G6B5.dds", 7210134307573079898, ImageFormat.R5g6b5)]
+        [InlineData("dds_R8G8B8.dds", 16481500256016389047, ImageFormat.Rgb24)]
+        [InlineData("dds_a1r5g5b5.dds", 12461131412352196151, ImageFormat.R5g5b5a1)]
+        [InlineData("dxt1-simple.dds", 17605116077597162586, ImageFormat.Rgb24)]
+        [InlineData("dxt3-simple.dds", 95660261486543378, ImageFormat.Rgba32)]
+        [InlineData("dxt5-simple-1x1.dds", 17927214943315913894, ImageFormat.Rgba32)]
+        [InlineData("dxt5-simple-odd.dds", 12356928648841185691, ImageFormat.Rgba32)]
+        [InlineData("dxt5-simple.dds", 95660261486543378, ImageFormat.Rgba32)]
+        [InlineData("flag_t32.tga", 7008764461524763534, ImageFormat.Rgba32)]
+        [InlineData("large-top-left.tga", 10870824044492905452, ImageFormat.Rgb24)]
+        [InlineData("marbles.tga", 10343037500488626953, ImageFormat.Rgb24)]
+        [InlineData("marbles2.tga", 14623019326840994965, ImageFormat.Rgb24)]
+        [InlineData("rgb24_top_left.tga", 4627684688687496028, ImageFormat.Rgb24)]
+        [InlineData("rgb24_top_left_colormap.tga", 4481644023605488430, ImageFormat.Rgb24)]
+        [InlineData("rgb32_top_left_rle_colormap.tga", 6150529532866763254, ImageFormat.Rgba32)]
+        [InlineData("tiny-rect.tga", 15893696507487264754, ImageFormat.Rgba32)]
+        [InlineData("true-24-bottom-right.tga", 1846039879287405346, ImageFormat.Rgb24)]
+        [InlineData("true-24-large.tga", 17691484392545185647, ImageFormat.Rgb24)]
+        [InlineData("true-24-rle.tga", 2765099695482911150, ImageFormat.Rgb24)]
+        [InlineData("true-24.tga", 17372466815898695035, ImageFormat.Rgb24)]
+        [InlineData("true-32-mixed.tga", 17866140955776085793, ImageFormat.Rgba32)]
+        [InlineData("true-32-rle-large.tga", 724744357722860486, ImageFormat.Rgba32)]
+        [InlineData("true-32-rle.tga", 2277897038200108277, ImageFormat.Rgba32)]
+        [InlineData("true-32.tga", 8563937048591661181, ImageFormat.Rgba32)]
+        [InlineData("wose_BC1_UNORM.DDS", 12101904599740452605, ImageFormat.Rgb24)]
+        [InlineData("wose_BC1_UNORM_SRGB.DDS", 10469302966092815447, ImageFormat.Rgb24)]
+        [InlineData("wose_R8G8B8A8_UNORM_SRGB.DDS", 18111997681897362439, ImageFormat.Rgba32)]
+        public void TestImageProperties(string path, ulong hash, ImageFormat format)
+        {
+            var data = File.ReadAllBytes(Path.Combine("data", path));
+            var image = Pfim.FromFile(Path.Combine("data", path));
+            var image2 = Path.GetExtension(path).ToLower() == ".dds"
+                ? (IImage)Dds.Create(data, new PfimConfig())
+                : Targa.Create(data, new PfimConfig());
+            image.ApplyColorMap();
+            image2.ApplyColorMap();
+            Assert.Equal(format, image.Format);
+            Assert.Equal(image.Format, image2.Format);
+            Assert.Equal(hash, Hash64(image.Data, image.Data.Length));
+            Assert.Equal(hash, Hash64(image2.Data, image2.Data.Length));
+        }
+    }
+}
---
+++ tests/Pfim.Tests/TargaTests.cs
@@ -0,0 +1,320 @@
+using System;
+using Xunit;
+using System.IO;
+
+namespace Pfim.Tests
+{
+    public class TargaTests
+    {
+        [Fact]
+        public void ParseTargaTrue24SingleColor()
+        {
+            byte[] expected = new byte[64 * 64 * 3];
+            for (int i = 0; i < expected.Length; i += 3)
+            {
+                expected[i] = 255;
+                expected[i + 1] = 176;
+                expected[i + 2] = 0;
+            }
+
+            var image = Pfim.FromFile(Path.Combine("data", "true-24.tga"));
+            Assert.Equal(expected, image.Data);
+        }
+
+        [Fact]
+        public void ParseTargaTrue32SingleColor()
+        {
+            byte[] expected = new byte[64 * 64 * 4];
+            for (int i = 0; i < expected.Length; i += 4)
+            {
+                expected[i] = 0;
+                expected[i + 1] = 0;
+                expected[i + 2] = 127;
+                expected[i + 3] = 255;
+            }
+
+            var image = Pfim.FromFile(Path.Combine("data", "true-32.tga"));
+            Assert.Equal(expected, image.Data);
+        }
+
+        [Fact]
+        public void ParseTarga32SingleSmallRunLength()
+        {
+            byte[] data = new byte[8];
+            byte[] stream = {129, 2, 4, 6, 8};
+            CompressedTarga.RunLength(data, stream, 0, 0, 4);
+            Assert.Equal(new byte[] {2, 4, 6, 8, 2, 4, 6, 8}, data);
+        }
+
+        [Fact]
+        public void ParseTarga24SingleSmallRunLength()
+        {
+            byte[] data = new byte[6];
+            byte[] stream = {129, 2, 4, 6};
+            CompressedTarga.RunLength(data, stream, 0, 0, 3);
+            Assert.Equal(new byte[] {2, 4, 6, 2, 4, 6}, data);
+        }
+
+        [Fact]
+        public void ParseTarga24RunLength()
+        {
+            byte[] data = new byte[18];
+            byte[] stream = {132, 2, 4, 6, 128, 8, 10, 12};
+            CompressedTarga.RunLength(data, stream, 0, 0, 3);
+            Assert.Equal(new byte[] {2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6, 0, 0, 0}, data);
+        }
+
+        [Fact]
+        public void ParseTarga32RunLength()
+        {
+            byte[] data = new byte[64 * 4];
+            int i = 0;
+            for (; i < 32 * 4; i += 4)
+            {
+                data[i] = 0;
+                data[i + 1] = 216;
+                data[i + 2] = 255;
+                data[i + 3] = 255;
+            }
+            for (; i < 32 * 4 + 16 * 4; i += 4)
+            {
+                data[i] = 255;
+                data[i + 1] = 148;
+                data[i + 2] = 0;
+                data[i + 3] = 255;
+            }
+            for (; i < 32 * 4 + 16 * 4 + 8 * 4; i += 4)
+            {
+                data[i] = 0;
+                data[i + 1] = 255;
+                data[i + 2] = 76;
+                data[i + 3] = 255;
+            }
+            for (; i < 32 * 4 + 16 * 4 + 8 * 4 + 8 * 4; i += 4)
+            {
+                data[i] = 0;
+                data[i + 1] = 0;
+                data[i + 2] = 255;
+                data[i + 3] = 255;
+            }
+
+            var image = Pfim.FromFile(Path.Combine("data", "true-32-rle.tga"));
+
+            Assert.Equal(data, image.Data);
+        }
+
+        [Fact]
+        public void ParseTarga24TrueRunLength()
+        {
+            byte[] data = new byte[64 * 3];
+            int i = 0;
+            for (; i < 32 * 3; i += 3)
+            {
+                data[i] = 0;
+                data[i + 1] = 216;
+                data[i + 2] = 255;
+            }
+            for (; i < 32 * 3 + 16 * 3; i += 3)
+            {
+                data[i] = 255;
+                data[i + 1] = 148;
+                data[i + 2] = 0;
+            }
+            for (; i < 32 * 3 + 16 * 3 + 8 * 3; i += 3)
+            {
+                data[i] = 0;
+                data[i + 1] = 255;
+                data[i + 2] = 76;
+            }
+            for (; i < 32 * 3 + 16 * 3 + 8 * 3 + 8 * 3; i += 3)
+            {
+                data[i] = 0;
+                data[i + 1] = 0;
+                data[i + 2] = 255;
+            }
+
+            var image = Pfim.FromFile(Path.Combine("data", "true-24-rle.tga"));
+
+            Assert.Equal(data, image.Data);
+        }
+
+        [Fact]
+        public void ParseUncompressedNonSquareTga()
+        {
+            var image = Pfim.FromFile(Path.Combine("data", "tiny-rect.tga"));
+            byte[] data = new byte[12 * 20 * 4];
+            for (int i = 0; i < data.Length; i += 4)
+            {
+                data[i] = 0;
+                data[i + 1] = 216;
+                data[i + 2] = 255;
+                data[i + 3] = 255;
+            }
+            Assert.Equal(data, image.Data);
+        }
+
+        [Fact]
+        public void ParseTrueTarga32MixedEncoding()
+        {
+            var image = Pfim.FromFile(Path.Combine("data", "true-32-mixed.tga"));
+            byte[] data = new byte[256];
+            for (int i = 0; i < 16 * 4; i += 4)
+            {
+                data[i] = 0;
+                data[i + 1] = 216;
+                data[i + 2] = 255;
+                data[i + 3] = 255;
+            }
+            Array.Copy(new byte[] {0, 0, 0, 255}, 0, data, 64, 4);
+            Array.Copy(new byte[] {64, 64, 64, 255}, 0, data, 68, 4);
+            Array.Copy(new byte[] {0, 0, 255, 255}, 0, data, 72, 4);
+            Array.Copy(new byte[] {0, 106, 255, 255}, 0, data, 76, 4);
+            Array.Copy(new byte[] {0, 216, 255, 255}, 0, data, 80, 4);
+            Array.Copy(new byte[] {0, 255, 182, 255}, 0, data, 84, 4);
+            Array.Copy(new byte[] {0, 255, 76, 255}, 0, data, 88, 4);
+            Array.Copy(new byte[] {33, 255, 0, 255}, 0, data, 92, 4);
+            Array.Copy(new byte[] {144, 255, 0, 255}, 0, data, 96, 4);
+            Array.Copy(new byte[] {255, 255, 0, 255}, 0, data, 100, 4);
+            Array.Copy(new byte[] {255, 148, 0, 255}, 0, data, 104, 4);
+            Array.Copy(new byte[] {255, 38, 0, 255}, 0, data, 108, 4);
+            Array.Copy(new byte[] {255, 0, 72, 255}, 0, data, 112, 4);
+            Array.Copy(new byte[] {255, 0, 178, 255}, 0, data, 116, 4);
+            Array.Copy(new byte[] {220, 0, 255, 255}, 0, data, 120, 4);
+            Array.Copy(new byte[] {110, 0, 255, 255}, 0, data, 124, 4);
+            for (int i = 128; i < 192; i += 4)
+            {
+                data[i] = 255;
+                data[i + 1] = 148;
+                data[i + 2] = 0;
+                data[i + 3] = 255;
+            }
+            for (int i = 192; i < 224; i += 4)
+            {
+                data[i] = 0;
+                data[i + 1] = 255;
+                data[i + 2] = 76;
+                data[i + 3] = 255;
+            }
+            for (int i = 224; i < 256; i += 4)
+            {
+                data[i] = 0;
+                data[i + 1] = 0;
+                data[i + 2] = 255;
+                data[i + 3] = 255;
+            }
+            Assert.Equal(data, image.Data);
+        }
+
+        [Fact]
+        public void ParseLarge32TargetImage()
+        {
+            var image = Pfim.FromFile(Path.Combine("data", "true-32-rle-large.tga"));
+            byte[] data = new byte[1200 * 1200 * 4];
+            for (int i = 0; i < data.Length; i += 4)
+            {
+                data[i] = 0;
+                data[i + 1] = 51;
+                data[i + 2] = 127;
+                data[i + 3] = 255;
+            }
+            Assert.Equal(data, image.Data);
+        }
+
+        [Fact]
+        public void ParseTargaTopLeft()
+        {
+            bool seenBlue = false;
+            var image = Pfim.FromFile(Path.Combine("data", "rgb24_top_left.tga"));
+            for (int i = 0; i < image.Data.Length; i += 3)
+            {
+                seenBlue |= image.Data[i] == 12 && image.Data[i + 1] == 0 && image.Data[i + 2] == 255;
+                if (image.Data[i] == 255 && image.Data[i + 1] == 4 && image.Data[i + 2] == 4 && !seenBlue)
+                {
+                    Assert.True(false, "Expected to see blue before red (this could mean that the color channels are swapped)");
+                }
+
+                if (!((image.Data[i] == 0 && image.Data[i + 1] == 255 && image.Data[i + 2] == 0) ||
+                      (image.Data[i] == 255 && image.Data[i + 1] == 0 && image.Data[i + 2] == 12) ||
+                      (image.Data[i] == 255 && image.Data[i + 1] == 255 && image.Data[i + 2] == 255) ||
+                      (image.Data[i + 2] == 255 && image.Data[i + 1] == image.Data[i])))
+                {
+                    Assert.True(false, $"Did not expect pixel {image.Data[i]} {image.Data[i + 1]} {image.Data[i + 2]}");
+                }
+            }
+        }
+
+        [Fact]
+        public void ParseTargaTopLeftColorMap()
+        {
+            var image = Pfim.FromFile(Path.Combine("data", "rgb24_top_left_colormap.tga"));
+            Assert.Equal(8, image.BitsPerPixel);
+            Assert.Equal(4096, image.Data.Length);
+            Assert.NotEqual(ImageFormat.Rgb24, image.Format);
+            image.ApplyColorMap();
+            Assert.Equal(ImageFormat.Rgb24, image.Format);
+            Assert.Equal(255, image.Data[0]);
+            Assert.Equal(255, image.Data[1]);
+            Assert.Equal(255, image.Data[2]);
+            Assert.Equal(255, image.Data[3]);
+            Assert.Equal(255, image.Data[4]);
+            Assert.Equal(255, image.Data[5]);
+        }
+
+        [Fact]
+        public void ParseTargaTopLeftRleStride()
+        {
+            var data = File.ReadAllBytes(Path.Combine("data", "DSCN1910_24bpp_uncompressed_10_2.tga"));
+            var image = Targa.Create(data, new PfimConfig());
+            Assert.Equal(461, image.Width);
+        }
+
+        [Fact]
+        public void ParseTargaTopLeftStride()
+        {
+            var data = File.ReadAllBytes(Path.Combine("data", "DSCN1910_24bpp_uncompressed_10_3.tga"));
+            var image = Targa.Create(data, new PfimConfig());
+            Assert.Equal(461, image.Width);
+            Assert.True(image.Data[460 * 3] != 0 && image.Data[461 * 3 + 1] != 0 && image.Data[461 * 3 + 2] != 0);
+        }
+
+        [Fact]
+        public void ParseLargeTargaTopLeft()
+        {
+            var image = Pfim.FromFile(Path.Combine("data", "large-top-left.tga"));
+            foreach (byte bt in image.Data)
+            {
+                Assert.Equal(0, bt);
+            }
+        }
+
+        [Fact]
+        public void ParseLargeTargaBottomLeft()
+        {
+            var image = Pfim.FromFile(Path.Combine("data", "marbles.tga"));
+            Assert.Equal(4264260, image.Data.Length);
+            Assert.Equal(0, image.Data[0]);
+            Assert.Equal(0, image.Data[1]);
+            Assert.Equal(0, image.Data[2]);
+        }
+
+        [Fact]
+        public void ParseMarblesTarga()
+        {
+            var image = Pfim.FromFile(Path.Combine("data", "marbles2.tga"));
+            Assert.Equal(100 * 71 * 3, image.Data.Length);
+            Assert.Equal(2, image.Data[0]);
+            Assert.Equal(3, image.Data[1]);
+            Assert.Equal(3, image.Data[2]);
+        }
+
+        [Fact]
+        public void ParseTransparentTarga()
+        {
+            var image = Pfim.FromFile(Path.Combine("data", "flag_t32.tga"));
+            for (int i = 0; i < image.Data.Length; i += 4)
+            {
+                Assert.Equal(0, image.Data[i + 3]);
+            }
+        }
+    }
+}
