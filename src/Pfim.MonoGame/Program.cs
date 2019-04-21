7487f2c4828c3da8fbc4029dcf972001b44a330e
Nick Babcock <nbabcock19@hotmail.com>
2019-04-20 19:10:42 -0500

    Add monogame sample

---
+++ src/Pfim.MonoGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pfim.MonoGame
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (var game = new MyGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
