1cbec3f91da2f972fef1e2231c887ca315c8758d
Nick Babcock <nbabcock19@hotmail.com>
2025-11-29 08:42:47 -0600

    Bump benchmark dependencies to latest

--- src/Pfim.Benchmarks/BaseConfig.cs
+++ src/Pfim.Benchmarks/BaseConfig.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Perfolizer.Horology;
+﻿using Perfolizer.Horology;
 using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Exporters.Csv;
-using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Reports;
+using Perfolizer.Metrology;
 
 namespace Pfim.Benchmarks
 {
